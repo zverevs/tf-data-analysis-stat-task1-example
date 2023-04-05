96a935b20119dcb7ed98708d30716ea520cd8b95
zverevs <129664415+zverevs@users.noreply.github.com>
2023-04-05 19:16:35 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,9 +5,13 @@ import numpy as np
 chat_id = 235789175 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    e = -17 + np.exp(1) 
-    m1 = np.mean(x)  
-    a = (m1 - e) / 10  
+    n = len(x)
+    
+    sample_mean = np.mean(x)
+
+    lambda_ = 1 / (sample_mean - 17)
+    
+    a = lambda_ * (1 - np.exp(-10*lambda_)) 
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     return a
