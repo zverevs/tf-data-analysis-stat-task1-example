62fb045be5e020fcd4a94d3f0bb230d5208a2ade
zverevs <129664415+zverevs@users.noreply.github.com>
2023-04-06 19:21:29 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,9 +5,8 @@ import numpy as np
 chat_id = 235789175 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    t= 10
-    а = (x.mean() + 16) / t*2
+    t = 10
 
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return a
+    return (x.mean() + 16) / t*2
